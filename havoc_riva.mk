d09ca12c59f927a6f49dffe3784ef2908428e5ff
Ankan005 <Ankan005@gmail.com>
2019-07-13 14:01:34 +0530

    bringup havoc

--- havoc_riva.mk
+++ havoc_riva.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -33,8 +33,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := aosp_riva
+PRODUCT_NAME := havoc_riva
 EXTENDED_BUILD_TYPE := Official
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.havoc.maintainer=Ankan005
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
